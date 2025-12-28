543a1d12e0ab0072ba077bd2ce9d72aed5a18c47
etherec0rd <13485627109@163.com>
2025-12-29 01:30:26 +0800

    fix: 配置 base 路径以适配子目录部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // 1. 你的 GitHub 用户名主页地址
+    site: 'https://etherec0rd.github.io',
+  
+    // 2. 关键！因为你没改仓库名，必须加上这个“尾巴”
+    // 必须和你的仓库名完全一致，开头加斜杠
+    base: '/Etherec0rD',
+});
